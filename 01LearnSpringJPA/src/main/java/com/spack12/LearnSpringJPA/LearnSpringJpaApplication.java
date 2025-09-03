fce4a71edc93f8006a61fac1371d53cc0a74dc76
spacky12 <spacky12@yahoo.com>
2025-09-04 02:36:50 +0530

    inital commit

---
+++ 01LearnSpringJPA/src/main/java/com/spack12/LearnSpringJPA/LearnSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.spack12.LearnSpringJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringJpaApplication.class, args);
+	}
+
+}
---
+++ 01LearnSpringJPA/src/test/java/com/spack12/LearnSpringJPA/LearnSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spack12.LearnSpringJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
