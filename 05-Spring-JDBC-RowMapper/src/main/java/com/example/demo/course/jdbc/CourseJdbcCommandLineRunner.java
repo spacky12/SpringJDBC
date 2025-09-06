c2cddeccb713d29bed7a0da2893b6cf81213e2f2
spacky12 <spacky12@yahoo.com>
2025-09-06 22:25:24 +0530

    RowMapper

---
+++ 05-Spring-JDBC-RowMapper/src/main/java/com/example/demo/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -0,0 +1,22 @@
+package com.example.demo.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.course.Course;
+
+@Component
+public class CourseJdbcCommandLineRunner implements CommandLineRunner{
+	
+	@Autowired
+	private CourseJdbcRepository repository;
+	@Override
+	public void run(String... args) throws Exception {
+		repository.insert(new Course(1, "Learn Java", "Neeraj"));
+		repository.insert(new Course(2, "Learn Spring", "Neeraj"));
+		repository.insert(new Course(3, "Learn SpringBoot", "Neeraj"));
+		repository.delete(3);
+		System.out.println(repository.select(2));
+	}
+}
